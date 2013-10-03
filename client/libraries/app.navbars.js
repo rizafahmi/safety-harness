fb7a307054e42e50b6c6b2cb5b7806a3ff59f2ab
Abigail Watson <awatson1978@gmail.com>
2013-10-03 03:11:54 -0400

    continuous integration timer works; features comparison table updated

--- client/libraries/app.navbars.js
+++ client/libraries/app.navbars.js
@@ -14,7 +14,7 @@ Template.navbarHeaderTemplate.getLink = function(){
 };
 
 
-Session.setDefault('integration_interval', 'off');
+Session.setDefault('integration_interval', false);
 Template.navbarFooterTemplate.events({
     'click .sample':function(){
         alert($('#iFrameBrowser').contents().find('title').html());
@@ -22,26 +22,50 @@ Template.navbarFooterTemplate.events({
 
   'click .1-minute':function(){
     Session.set('integration_interval', 1);
+    setIntegrationInterval (60);
   },
   'click .5-minute':function(){
     Session.set('integration_interval', 5);
+    setIntegrationInterval(300);
   },
   'click .15-minute':function(){
     Session.set('integration_interval', 15);
+    setIntegrationInterval(900);
   },
   'click .30-minute':function(){
     Session.set('integration_interval', 30);
+    setIntegrationInterval(1800);
   },
   'click .60-minute':function(){
     Session.set('integration_interval', 60);
+    setIntegrationInterval(3600);
   },
   'click .240-minute':function(){
     Session.set('integration_interval', 240);
+    setIntegrationInterval(14400);
   },
   'click .1440-minute':function(){
     Session.set('integration_interval', 1440);
+    setIntegrationInterval(864000);
   }
 });
+setIntegrationInterval = function(interval) {
+  Session.set ('remaining_time_in_seconds', interval);
+  setInterval (function () {
+    if(Session.get('is_timer_active')){
+      if(Session.get('remaining_time_in_seconds') > 0){
+        Session.set('remaining_time_in_seconds', Session.get('remaining_time_in_seconds') - 1);
+      }else{
+        Session.set('remaining_time_in_seconds', interval);
+        console.log('time to integrate!');
+        alert('Time to run integration tests!  Unfortunately, this feature is only half-implemented, so only pops up this message.');
+        Meteor.flush();
+      }
+    }
+  }, 1000);
+}
+
+
 
 Template.navbarFooterTemplate.getMinuteStyle1 = function(){
   if(Session.get('integration_interval') === 1) {
@@ -96,6 +120,9 @@ Template.navbarFooterTemplate.getMinuteStyle1440 = function(){
 Template.navbarFooterTemplate.getIntegrationInterval = function(){
   return Session.get('integration_interval');
 }
+Template.navbarFooterTemplate.getRemainingTime = function(){
+  return Session.get('remaining_time_in_seconds');
+}
 
 Session.setDefault('is_timer_active', false);
 Template.navbarFooterTemplate.getTimerButtonText = function(){
@@ -114,14 +141,19 @@ Template.navbarFooterTemplate.isIntegrationActivated = function(){
 };
 Template.navbarFooterTemplate.events({
     'click .timer-button': function(){
-        toggleSession('is_timer_active');
+      if(Session.get('is_timer_active')){
+        Session.set('integration_interval', 'off');
+        Session.set('is_timer_active', false);
+      }else{
+        Session.set('is_timer_active', true);
+      }
     }
 })
 
-toggleSession = function(session_variable){
-    if(Session.get(session_variable)){
-        Session.set(session_variable, false);
-    }else{
-        Session.set(session_variable, true);
-    }
-}
+//toggleSession = function(session_variable){
+//    if(Session.get(session_variable)){
+//        Session.set(session_variable, false);
+//    }else{
+//        Session.set(session_variable, true);
+//    }
+//}
